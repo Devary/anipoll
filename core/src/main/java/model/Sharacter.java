65f7a9fca7b607534c8284e79266055658aa6c3b
Fakher H. <hfekher@gmail.com>
2025-09-09 15:48:47 +0100

    Use anime IDs in sharacter model

--- core/src/main/java/model/Sharacter.java
+++ core/src/main/java/model/Sharacter.java
@@ -4,13 +4,15 @@ import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.EqualsAndHashCode;
 import lombok.NoArgsConstructor;
-import javax.json.bind.annotation.JsonbTransient;
 
 @Data
 @EqualsAndHashCode(callSuper = true)
 @NoArgsConstructor
 @AllArgsConstructor
 public class Sharacter extends BaseEntity {
-    @JsonbTransient
-    private Anime anime;
+    /**
+     * Identifier of the {@link Anime} this sharacter belongs to. Only the id is
+     * stored to avoid holding a full object reference.
+     */
+    private Long animeId;
 }
