ce6df9eb81809a01ae8c710674d967269a29f8e1
fhm <hfekher@gmail.com>
2025-09-09 09:54:45 +0100

    first-commit

---
+++ service-template/src/main/java/com/anipoll/App.java
@@ -0,0 +1,13 @@
+package com.anipoll;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
